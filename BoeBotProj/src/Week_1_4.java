3215c93d2c23b5fe3a39a7212d9eca20e6c7b32e
Jonathan <Jonathan_Van_Waardenburg@hotmail.com>
2019-11-07 13:47:30 +0100

    Week_1_4c

--- BoeBotProj/src/Week_1_4.java
+++ BoeBotProj/src/Week_1_4.java
@@ -6,24 +6,41 @@ public class Week_1_4 {
 
         Timer t1 = new Timer(1000);
         Timer t2 = new Timer(1000);
-        Timer t3 = new Timer(700);
+        Timer t3 = new Timer(1300);
         Timer t4 = new Timer(700);
         t1.mark();
         t3.mark();
+
+        BoeBot.digitalWrite(2, false);
+        BoeBot.digitalWrite(7, true);
 
         while (true)
         {
             if (!BoeBot.digitalRead(10)) {
                 if (t1.timeout()) {
                     BoeBot.digitalWrite(7, !BoeBot.digitalRead(7));
+                    BoeBot.digitalWrite(2, !BoeBot.digitalRead(7));
                     t2.mark();
                 }
                 if (t2.timeout()) {
                     BoeBot.digitalWrite(7, !BoeBot.digitalRead(7));
+                    BoeBot.digitalWrite(2, !BoeBot.digitalRead(7));
                     t1.mark();
                 }
             }
-            else if (BoeBot.digitalRead(10)) {
+            else {
+                if (t3.timeout()) {
+                    BoeBot.digitalWrite(7, !BoeBot.digitalRead(7));
+                    BoeBot.digitalWrite(2, !BoeBot.digitalRead(7));
+                    t4.mark();
+                }
+                if (t4.timeout()) {
+                    BoeBot.digitalWrite(7, !BoeBot.digitalRead(7));
+                    BoeBot.digitalWrite(2, !BoeBot.digitalRead(7));
+                    t3.mark();
+                }
+            }
+            /*else if (BoeBot.digitalRead(10)) {
                 BoeBot.digitalWrite(7, false);
             }
             if (!BoeBot.digitalRead(9)) {
@@ -38,7 +55,7 @@ public class Week_1_4 {
             }
             else if (BoeBot.digitalRead(9)) {
                 BoeBot.digitalWrite(2, false);
-            }
+            }*/
             BoeBot.wait(1);
         }
     }
