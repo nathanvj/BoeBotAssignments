cde76b2a8258b626dd3564e4bf41c3ad9bc1eaa9
Jonathan <Jonathan_Van_Waardenburg@hotmail.com>
2019-11-07 11:03:50 +0100

    Week_1_2a

---
+++ BoeBotProj/src/Week_1_2.java
@@ -0,0 +1,26 @@
+import TI.BoeBot;
+import TI.Timer;
+
+public class Week_1_2 {
+
+    public static void main(String[] args) {
+
+        Timer t1 = new Timer(1000);
+        Timer t2 = new Timer(1000);
+
+        t1.mark();
+        t2.mark();
+        while (true) {
+
+            if (t1.timeout()) {
+                BoeBot.digitalWrite(8, !BoeBot.digitalRead(8));
+                t1.mark();
+            }
+            if (t2.timeout()) {
+                BoeBot.digitalWrite(13, !BoeBot.digitalRead(13));
+                t2.mark();
+            }
+            BoeBot.wait(1);
+        }
+    }
+}
