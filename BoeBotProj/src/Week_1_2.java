c25befa3ca5ec379cbe2acfdc0c7ccb9c45f1e1d
Jonathan <Jonathan_Van_Waardenburg@hotmail.com>
2019-11-07 11:28:30 +0100

    Week_1_2e

--- BoeBotProj/src/Week_1_2.java
+++ BoeBotProj/src/Week_1_2.java
@@ -6,20 +6,30 @@ public class Week_1_2 {
     public static void main(String[] args) {
 
         Timer t1 = new Timer(1300);
-        Timer t2 = new Timer(700);
+        Timer t2 = new Timer(800);
+        Timer t1Uit = new Timer(700);
+        Timer t2Uit = new Timer(1700);
 
         t1.mark();
         t2.mark();
 
-        BoeBot.digitalWrite(8, false);
-        BoeBot.digitalWrite(13, false);
+        BoeBot.digitalWrite(8, true);
+        BoeBot.digitalWrite(13, true);
         while (true) {
 
             if (t1.timeout()) {
                 BoeBot.digitalWrite(8, !BoeBot.digitalRead(8));
-                t1.mark();
+                t1Uit.mark();
             }
             if (t2.timeout()) {
+                BoeBot.digitalWrite(13, !BoeBot.digitalRead(13));
+                t2Uit.mark();
+            }
+            if (t1Uit.timeout()) {
+                BoeBot.digitalWrite(8, !BoeBot.digitalRead(8));
+                t1.mark();
+            }
+            if (t2Uit.timeout()) {
                 BoeBot.digitalWrite(13, !BoeBot.digitalRead(13));
                 t2.mark();
             }
