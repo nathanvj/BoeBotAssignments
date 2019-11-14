e3686b08abad6fc158df472d2968ed325ac6989f
Jonathan <Jonathan_Van_Waardenburg@hotmail.com>
2019-11-14 09:38:41 +0100

    Week_2_1a

---
+++ BoeBotProj/src/Week_2_1.java
@@ -0,0 +1,14 @@
+import TI.*;
+
+public class Week_2_1 {
+    public Week_2_1() {
+    }
+
+    public static void main(String[] args) {
+
+        PWM pwm = new PWM(8, 128);
+        while(true) {
+
+        }
+    }
+}
