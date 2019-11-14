644e8d63ae07dc40b9a0f31e8922e3ca72feadbc
Jonathan <Jonathan_Van_Waardenburg@hotmail.com>
2019-11-14 15:12:15 +0100

    Week_2_7b

--- BoeBotProj/src/Week_2_7.java
+++ BoeBotProj/src/Week_2_7.java
@@ -4,9 +4,15 @@ public class Week_2_7 {
 
     public static void main(String[] args) {
 
-        BoeBot.freqOut(8, 1000, 5000);
+
         while(true) {
 
+            for(int i =1000;i<=2000;i++) {
+                BoeBot.freqOut(8, i, 1);
+            }
+            for(int i =2000;i>=1000;i--) {
+                BoeBot.freqOut(8, i, 1);
+            }
         }
     }
 }
