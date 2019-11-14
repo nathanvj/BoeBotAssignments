9b0d1d0cef2ea52a22fb098069d21e1c4b131f7b
Jonathan <Jonathan_Van_Waardenburg@hotmail.com>
2019-11-14 10:42:11 +0100

    Week_2_4a

---
+++ BoeBotProj/src/Week_2_4.java
@@ -0,0 +1,17 @@
+import TI.*;
+
+public class Week_2_4 {
+
+    public static void main(String[] args) {
+
+        Servo s1 = new Servo(12);
+        Servo s2 = new Servo(13);
+
+        s1.update(1500);
+        s2.update(1500);
+
+        while(true) {
+
+        }
+    }
+}
