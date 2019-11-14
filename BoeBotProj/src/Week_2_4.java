1bba04c514865d81efc8eab070c18b4a185f2555
Jonathan <Jonathan_Van_Waardenburg@hotmail.com>
2019-11-14 11:16:54 +0100

    Week_2_4b update

--- BoeBotProj/src/Week_2_4.java
+++ BoeBotProj/src/Week_2_4.java
@@ -24,17 +24,6 @@ public class Week_2_4 {
             s1.update(1500);
             s2.update(1500);
 
-            for(int i = 0; i <= 200; i += 10) {
-                s1.update(1500-i);
-                s2.update(1500+i);
-                System.out.println("S1: "+(1500+i));
-                System.out.println("S2: "+(1500-i));
-                BoeBot.wait(2000);
-
-            }
-            s1.update(1500);
-            s2.update(1500);
-
             duration = false;
 
         }
