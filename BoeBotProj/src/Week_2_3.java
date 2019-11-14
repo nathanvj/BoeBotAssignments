4eb520ef814c00b68fa12da290d12cffc55558c3
Jonathan <Jonathan_Van_Waardenburg@hotmail.com>
2019-11-14 10:24:02 +0100

    Week_2_3a

---
+++ BoeBotProj/src/Week_2_3.java
@@ -0,0 +1,55 @@
+import TI.*;
+
+public class Week_2_3 {
+
+    public static void main(String[] args) {
+
+        final int redPin = 6;
+        final int greenPin = 7;
+        final int bluePin = 8;
+
+        while(true) {
+
+            BoeBot.digitalWrite(redPin, false);
+            BoeBot.digitalWrite(greenPin, false);
+            BoeBot.digitalWrite(bluePin, false);
+            BoeBot.wait(1000);
+
+            BoeBot.digitalWrite(redPin, true);
+            BoeBot.digitalWrite(greenPin, false);
+            BoeBot.digitalWrite(bluePin, false);
+            BoeBot.wait(1000);
+
+            BoeBot.digitalWrite(redPin, false);
+            BoeBot.digitalWrite(greenPin, true);
+            BoeBot.digitalWrite(bluePin, false);
+            BoeBot.wait(1000);
+
+            BoeBot.digitalWrite(redPin, false);
+            BoeBot.digitalWrite(greenPin, false);
+            BoeBot.digitalWrite(bluePin, true);
+            BoeBot.wait(1000);
+
+            BoeBot.digitalWrite(redPin, true);
+            BoeBot.digitalWrite(greenPin, true);
+            BoeBot.digitalWrite(bluePin, false);
+            BoeBot.wait(1000);
+
+            BoeBot.digitalWrite(redPin, true);
+            BoeBot.digitalWrite(greenPin, false);
+            BoeBot.digitalWrite(bluePin, true);
+            BoeBot.wait(1000);
+
+            BoeBot.digitalWrite(redPin, false);
+            BoeBot.digitalWrite(greenPin, true);
+            BoeBot.digitalWrite(bluePin, true);
+            BoeBot.wait(1000);
+
+            BoeBot.digitalWrite(redPin, true);
+            BoeBot.digitalWrite(greenPin, true);
+            BoeBot.digitalWrite(bluePin, true);
+            BoeBot.wait(1000);
+            
+        }
+    }
+}
